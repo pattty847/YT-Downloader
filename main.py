45496bd09f88cb04ee80e55b75eb2b7442375169
Patrick <pattty847@gmail.com>
2023-01-24 16:21:58 -0500

    Added download progress updates, audio, video, both download options.

--- main.py
+++ main.py
@@ -56,24 +56,22 @@ def on_progress(stream, chunk, bytes_remaining):
     total_size = stream.filesize
     bytes_downloaded = total_size - bytes_remaining
     pct_completed = bytes_downloaded / total_size * 100
-    print(f"Status: {round(pct_completed, 2)} %")
+    dpg.add_text(f"Status: {round(pct_completed, 2)} %", parent="downloading")
 
 
 def on_complete():
     pass
 
 
-def save_videos(new_folder_name, our_links, download_type):
+def save_videos(saved_path, our_links, download_type, convert):
 
     # Create the folder the downloads will go in
     try:
-        os.mkdir(new_folder_name)
+        os.mkdir(saved_path)
     except:
         print('Folder exists.')
 
-    os.chdir(new_folder_name)
-    SAVEPATH = "Audio Files"
-    dpg.add_text(f'Files will be saved to: {new_folder_name}', parent="downloading")
+    dpg.add_text(f'Files will be saved to: {saved_path}', parent="downloading")
 
     dpg.add_text('Connecting to YouTube API...', parent="downloading")
 
@@ -83,6 +81,7 @@ def save_videos(new_folder_name, our_links, download_type):
             yt = YouTube(link, on_progress_callback=on_progress)
             main_title = yt.title
             main_title = main_title.replace('|', '')
+
             
         except:
             dpg.add_text('Connection issue.', parent="downloading")
@@ -91,16 +90,19 @@ def save_videos(new_folder_name, our_links, download_type):
         
         try:
             if download_type == "Audio Only":
-                vid = yt.streams.filter(only_audio=True).first()
-            elif download_type == "Video Only":
-                vid = yt.streams.filter(only_video=True).first()
-            elif download_type == "Both":
-                vid = yt.streams.filter(only_audio=True).first()
+                vid = yt.streams.get_audio_only()
+                vid.download(saved_path, filename=main_title+'.mp3' if convert else '.mp4')
 
-                
-            dpg.add_text('Downloading - ' + vid.default_filename + ' - File Size -> ' + str(round(vid.filesize / (1024 * 1024), 2)) + ' MB.', parent="downloading")
-            vid.download(SAVEPATH)
-            dpg.add_text('Complete', parent="downloading")
+            elif download_type == "Video Only":
+                vid = yt.streams.filter(only_video=True)
+                vid.download(saved_path, filename=main_title+'.mp3' if convert else '.mp4')
+            elif download_type == "Both":
+                vid = yt.streams.filter(progressive=True)
+                vid.download(saved_path, filename=main_title+'.mp3' if convert else '.mp4')
+
+
+            dpg.add_text(f"Finished: " + main_title, parent="downloading")
+
 
         except Exception as e:
             print(e)
@@ -108,7 +110,7 @@ def save_videos(new_folder_name, our_links, download_type):
 
     
     dpg.add_text('Finished.', parent="downloading")
-    dpg.add_text(f'They can be found at: downloads/{SAVEPATH}', parent="downloading")
+    dpg.add_text(f'They can be found at: {saved_path}', parent="downloading")
 
 
 def monitors():
@@ -125,7 +127,7 @@ def download(sender, app_data, user_data):
 
     if str(URL).startswith("https://www.youtube.com"):
         links = link_snatcher(URL)
-        save_videos("downloads", links, DOWNLOAD_TYPE)
+        save_videos("Downloads", links, DOWNLOAD_TYPE, user_data[2])
     else:
         dpg.add_text("Link must begin with 'https://www.youtube.com'.", parent="downloading")
 
@@ -134,34 +136,47 @@ def download_type_label(sender, app_data, user_data):
     dpg.set_value("download-type-label", "Download Type: " + app_data)
 
 
+def convert_type_label(sender, app_data, user_data):
+    dpg.set_value("convert-type-label", app_data)
+
+
 def start_program():
     
     dpg.create_context()
 
     with dpg.window(label="Julian", tag="Primary Window"):
         with dpg.viewport_menu_bar():
-            with dpg.menu(label="Settings"):
-                with dpg.menu(label="Download Type"):
-                    download_type = dpg.add_radio_button(("Audio Only", "Video Only", "Both"), default_value="Audio Only" , horizontal=True, callback=download_type_label)
 
+            with dpg.menu(label="Settings"):
+
+                with dpg.menu(label="Download Type"):
+
+                    download_type = dpg.add_radio_button(("Audio Only", "Video Only", "Both"), default_value="Audio Only" , horizontal=True, callback=download_type_label)
+                
+                conversion_type = dpg.add_checkbox(label="Convert to MP3?", default_value=True)
             dpg.add_menu_item(label="Demo", callback=lambda: demo.show_demo())
 
         with dpg.child_window(pos=(0, 25)):
 
             dpg.add_text("Download Type: " + dpg.get_value(download_type), tag="download-type-label")
+            dpg.add_text("Convert to MP3?: " + "True" if dpg.get_value(conversion_type) else "False", tag="convert-type-label")
+
 
             URL = dpg.add_input_text(label="YouTube Playlist URL", width=425)
 
-            dpg.add_button(label="Download", callback = download, user_data=(URL, download_type))
+            dpg.add_button(label="Download", callback = download, user_data=(URL, download_type, conversion_type))
 
             with dpg.child_window(tag="downloading"):
                 pass
 
 
     # Primary Window: x and y pos
-    x, y = monitors().x, monitors().y
+    x, y, width, height = monitors().x, monitors().y, monitors().width, monitors().height
+    # Centered viewport pos
+    x = x + int(width/2-600)
+    y = y + int(height/2-500)
 
-    dpg.create_viewport(title='YouTube Playlist Downloader', x_pos=x, y_pos=y, width=600, height=200, resizable=False)
+    dpg.create_viewport(title='YouTube Playlist Downloader', x_pos=x, y_pos=y, width=600, height=500, resizable=False)
     dpg.setup_dearpygui()
     dpg.show_viewport()
     dpg.set_primary_window("Primary Window", True)
